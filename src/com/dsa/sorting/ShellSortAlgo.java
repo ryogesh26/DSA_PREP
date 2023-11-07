1cf04b7688615965ee420cc9e222c90a47cc197f
Yogisgit <yogeshravi2704@gmail.com>
2023-11-07 17:46:58 +0530

    Shell sort algorithm

---
+++ src/com/dsa/sorting/ShellSortAlgo.java
@@ -0,0 +1,30 @@
+package com.dsa.sorting;
+
+public class ShellSortAlgo {
+
+	public static void main(String[] args) {
+		int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+		for (int gap = intArray.length / 2; gap > 0; gap = gap / 2) {
+
+			for (int i = gap; i < intArray.length; i++) {
+
+				int j = i;
+				int newElement = intArray[i];
+
+				while (j >= gap && intArray[j - gap] > newElement) {
+					intArray[j] = intArray[j - gap];
+					j = j - gap;
+				}
+				intArray[j] = newElement;
+			}
+
+		}
+
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+
+	}
+
+}
